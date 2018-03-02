ab239b0d5eb8b81e04ecf96d7bf2b2743e32b700
Stefan Holm Olsen <stefan.holm.olsen@gmail.com>
2018-03-02 15:39:42 +0100

    Add inbound extension and catalog document.

---
+++ StefanOlsen.InRiver.MappedImporter/CatalogDocument.cs
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2018 Stefan Holm Olsen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.XPath;
+using inRiver.Remoting;
+
+namespace StefanOlsen.InRiver.MappedImporter
+{
+    public class CatalogDocument
+    {
+        private readonly IinRiverManager _inRiverManager;
+        private XPathDocument _xPathDocument;
+
+
+        private bool _isInitialized;
+
+        public CatalogDocument(IinRiverManager inRiverManager)
+        {
+            _inRiverManager = inRiverManager;
+        }
+
+        public void Initialize(string mappingDocument, string dataDocument)
+        {
+            if (_isInitialized)
+            {
+                throw new InvalidOperationException($"This {nameof(CatalogDocument)} is already initialized.");
+            }
+
+            InitializeMapping(mappingDocument);
+            LoadData(dataDocument);
+
+            _isInitialized = true;
+        }
+
+        private void InitializeMapping(string mappingDocument)
+        {
+
+        }
+
+        private void LoadData(string dataDocument)
+        {
+            Stream dataStream = GetStream(dataDocument);
+            _xPathDocument = new XPathDocument(dataStream);
+        }
+
+        private static Stream GetStream(string data)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(data);
+            Stream stream = new MemoryStream(bytes, false);
+
+            return stream;
+        }
+    }
+}
---
+++ StefanOlsen.InRiver.MappedImporter/MappedInboundExtension.cs
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2018 Stefan Holm Olsen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using inRiver.Remoting.Extension;
+using inRiver.Remoting.Extension.Interface;
+
+namespace StefanOlsen.InRiver.MappedImporter
+{
+    public class MappedInboundExtension : IInboundDataExtension
+    {
+        public inRiverContext Context { get; set; }
+        public Dictionary<string, string> DefaultSettings => new Dictionary<string, string>
+        {
+            {"MAPPING_CONFIGURATION_XML", "Insert XML mapping here" }
+        };
+
+        public string Test()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Add(string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Update(string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Delete(string value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
